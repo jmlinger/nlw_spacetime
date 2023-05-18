0e1c0f4e6e06f9fe22477fab0bef176b00555eca
jmlinger <jmlinger10@gmail.com>
2023-05-18 12:26:51 -0300

    build and feat: it starts mobile project.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-950">
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )

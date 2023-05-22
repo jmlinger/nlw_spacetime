71db727091c9878c0cfaa23a400d317e822aa531
jmlinger <jmlinger10@gmail.com>
2023-05-22 17:32:49 -0300

    feat: it creates Sign in, Hero and Copyrights elements.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="itens-center flex-1 justify-center bg-gray-950">
+    <View className="flex-1 items-center justify-center bg-gray-950">
       <Text className="text-5xl font-bold text-zinc-50">Hello World</Text>
       <StatusBar style="light" translucent />
     </View>
